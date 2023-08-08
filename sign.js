6185a72df24acfa4a5bbef5aa56ba79a18693ac2
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2023-08-08 22:08:03 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,45 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+
+    })
+}
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+
+    })
+}
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+  const wrapper = document.querySelector(".wrapper"),
+  signupHeader = document.querySelector(".signup header"),
+  loginHeader = document.querySelector(".login header");
+
+  
+  loginHeader.addEventListener("click",()=>{
+      wrapper.classList.add("active");
+  });
+  signupHeader.addEventListener("click",()=>{
+      wrapper.classList.remove("active");
+  });
